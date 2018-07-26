9c04778d31dcf81cd66ee466b50bc10f2e9d0f5c
Ming Jiang <minkyn@users.noreply.github.com>
2018-07-26 12:25:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,16 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 brew tap caskroom/cask
 
+# brew cask install adobe-acrobat-pro
 brew cask install caffeine
 # brew cask install elmedia-player
 # brew cask install folx
 # brew cask install google-chrome
+brew cask install iterm2
 brew cask install java
+# brew cask install microsoft-office
 brew cask install shadowsocksx-ng
+# brew cask install visual-studio-code
 brew cask install xquartz
 
 brew cask cleanup
