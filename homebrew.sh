a322fef94c335f4c2732b10e08a83e58c87e1bf5
Ming Jiang <minkyn@users.noreply.github.com>
2019-06-10 08:16:51 +0800

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -91,6 +91,7 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # brew install ffmpeg
+# brew install glew
 # brew install glfw
 # brew install gstreamer
 # brew install mysql
