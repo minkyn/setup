7ca0d20fa02eb7aa8f0c283fef32c19d3dff659e
Ming Jiang <minkyn@users.noreply.github.com>
2020-02-09 18:56:58 +0800

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -32,6 +32,7 @@ brew cask install xmind-zen
 brew cask install visual-studio-code
 brew cask install github
 brew cask install miniconda
+# brew cask install sequel-pro
 # brew cask install postman
 # brew cask install wireshark
 brew cask install iterm2
