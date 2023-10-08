cf2fa3617adaffc083e28853a0ea8bbb1fc186c0
Ming Jiang <minkyn@users.noreply.github.com>
2023-10-09 01:12:28 +0800

    Update homebrew.sh

--- homebrew.sh
+++ homebrew.sh
@@ -19,9 +19,8 @@ brew install --cask feishu
 brew install --cask wechatwork
 brew install --cask whatsapp
 brew install --cask telegram
-# brew install --cask slack
-# brew install --cask discord
-
+brew install --cask slack
+brew install --cask discord
 brew install --cask tecent-meeting
 brew install --cask zoom
 # brew install --cask microsoft-teams
@@ -29,49 +28,38 @@ brew install --cask zoom
 brew install --cask baidunetdisk
 # brew install --cask google-drive
 # brew install --cask onedrive
-
 brew install --cask notion
 brew install --cask figma
-# mas install trello
-
-brew install --cask adobe-acrobat-pro  # adobe-acrobat-reader
+brew install --cask adobe-acrobat-pro
 brew install --cask microsoft-office
 # mas install iWork
-
 brew install --cask xmind-zen
-# brew install --cask drawio
-# brew install --cask mactex
+brew install --cask mactex-no-gui  # tlmgr update
+# mas install zlibrary
 
-# mas install affinity-photo
-# mas install affinity-designer
-# mas install final-cut-pro motion compressor
-# mas install logic-pro main-stage guitar-pro
-# mas install imovie garage-band
-# brew install --cask musescore
-# brew install --cask blender
+# mas install affinity-photo affinity-designer
+# mas install final-cut-pro compressor motion
+# mas install logic-pro garage-band main-stage
+brew install --cask blender
+brew install --cask musescore
 
 brew install --cask visual-studio-code
 # mas install xcode
 # brew install --cask android-studio
 # brew install --cask scratch
-
-brew install --cask postman  # paw insomnia
-brew install --cask tableplus  # sequel-ace
-
+brew install --cask miniconda  # conda init zsh
+brew install --cask postman
+brew install --cask tableplus
 brew install --cask github
 brew install --cask iterm2
-
 brew install --cask v2rayu
 brew install --cask shadowsocksx-ng-r
 # brew install --cask openvpn-connect
 brew install --cask proxifier
 # brew install --cask wireshark
 
-brew install --cask miniconda
-# conda init zsh
-
 brew install --cask xquartz
-# defaults write org.macosforge.xquartz.X11 enable_iglx -bool true
+defaults write org.xquartz.X11 enable_iglx -bool true
 
 brew install autoconf
 brew install automake
