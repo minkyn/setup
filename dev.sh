d0a22ba084aa7214a672e28006bc9c96dac493cb
Ming Jiang <minkyn@users.noreply.github.com>
2024-03-26 21:36:40 +0800

    Update dev.sh

--- dev.sh
+++ dev.sh
@@ -5,28 +5,36 @@ code --install-extension ms-python.python
 code --install-extension ms-python.black-formatter
 code --install-extension ms-toolsai.jupyter
 code --install-extension rust-lang.rust-analyzer
+
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
 code --install-extension bradlc.vscode-tailwindcss
 code --install-extension ms-vscode.live-server
-code --install-extension dart-code.flutter
-code --install-extension vue.volar
+code --install-extension Dart-Code.flutter
+code --install-extension Vue.volar
 code --install-extension sswg.swift-lang
-code --install-extension goland.go
+
+code --install-extension golang.go
 code --install-extension vscjava.vscode-java-pack
 code --install-extension ms-dotnettools.csdevkit
 code --install-extension ms-vscode.cpptools-extension-pack
+
 code --install-extension Arm.keil-studio-pack
 code --install-extension NomicFoundation.hardhat-solidity
 code --install-extension ms-vscode.hexeditor
+
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension James-Yu.latex-workshop
+
 code --install-extension figma.figma-vscode-extension
 code --install-extension Postman.postman-for-vscode
 code --install-extension mtxr.sqltools
-code --install-extension yzhang.markdown-all-in-one
-code --install-extension James-Yu.latex-workshop
+
+code --install-extension GitHub.remotehub
 code --install-extension GitHub.vscode-pull-request-github
-code --install-extension eamodio.gitlens
+code --install-extension github.vscode-github-actions
 code --install-extension GitHub.copilot
+code --install-extension eamodio.gitlens
 
 # Environments
 brew install bazelisk
